26b0a9acb401b251e684deebe48f11b4944cd3f6
pajamaw <pjpeterww@gmail.com>
2016-03-15 15:12:03 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(ice_cream){
+  return "I love " + ice_cream
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
